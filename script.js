0953009ca7cce30d11f4b350399527d9e0b74031
Imrul Anwar <imrulanwar836@gmail.com>
2023-06-30 19:17:18 +0600

    Handled Errors

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const temperature = document.getElementById("temperature");
 const weatherDesc = document.getElementById("weatherDesc");
 const humidity = document.getElementById("humidity");
 const windSpeed = document.getElementById("windSpeed");
+const locationNotFound = document.getElementById("lnf");
+const weatherBody = document.getElementById("weatherBody");
 
 const API_KEY = "9a8540a130c68d7453a8e576039c5b29";
 
@@ -15,34 +17,13 @@ async function checkWeather(city) {
     const weather_response = await fetch(URL).then((response) =>
       response.json()
     );
-    console.log(weather_response);
-    temperature.innerHTML =
-      Math.round(weather_response.main.temp - 273.15) + "°C";
-    weatherDesc.innerHTML = weather_response.weather[0].description;
-    humidity.innerHTML = weather_response.main.humidity + " %";
-    windSpeed.innerHTML = weather_response.wind.speed + "km/h";
-
-    switch (weather_response.weather[0].main) {
-      case "Clouds":
-        weatherImg.src = "images/cloud.png";
-        break;
-
-      case "Clear":
-        weatherImg.src = "images/clear.png";
-        break;
-
-      case "Mist":
-        weatherImg.src = "images/mist.png";
-        break;
-
-      case "Rain":
-        weatherImg.src = "images/rain.png";
-        break;
-
-      case "Snow":
-        weatherImg.src = "images/snow.png";
-        break;
+    if (weather_response.cod === "404") {
+      locationNotFound.style.display = "flex";
+      weatherBody.style.display = "none";
+      console.log("error");
+      return;
     }
+    setTheValues(weather_response);
   } catch (error) {
     console.log(error);
   }
@@ -51,3 +32,35 @@ async function checkWeather(city) {
 searchBtn.addEventListener("click", () => {
   checkWeather(inputBox.value);
 });
+function setTheValues(weather_response) {
+  console.log(weather_response);
+  temperature.innerHTML =
+    Math.round(weather_response.main.temp - 273.15) + "°C";
+  weatherDesc.innerHTML = weather_response.weather[0].description;
+  humidity.innerHTML = weather_response.main.humidity + " %";
+  windSpeed.innerHTML = weather_response.wind.speed + "km/h";
+  locationNotFound.style.display = "none";
+  weatherBody.style.display = "flex";
+
+  switch (weather_response.weather[0].main) {
+    case "Clouds":
+      weatherImg.src = "images/cloud.png";
+      break;
+
+    case "Clear":
+      weatherImg.src = "images/clear.png";
+      break;
+
+    case "Mist":
+      weatherImg.src = "images/mist.png";
+      break;
+
+    case "Rain":
+      weatherImg.src = "images/rain.png";
+      break;
+
+    case "Snow":
+      weatherImg.src = "images/snow.png";
+      break;
+  }
+}
