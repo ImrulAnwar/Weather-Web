6973119b3eeefd8f872849f80933c4b0ca8e6540
Imrul Anwar <imrulanwar836@gmail.com>
2023-06-30 19:24:34 +0600

    Project Completed

--- script.js
+++ script.js
@@ -10,38 +10,48 @@ const weatherBody = document.getElementById("weatherBody");
 
 const API_KEY = "9a8540a130c68d7453a8e576039c5b29";
 
+searchBtn.addEventListener("click", () => {
+  checkWeather(inputBox.value);
+});
+
 async function checkWeather(city) {
   const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
 
   try {
+    // API call to get weather
     const weather_response = await fetch(URL).then((response) =>
       response.json()
     );
+    // Check if the response throws an error
     if (weather_response.cod === "404") {
+      // Hiding the weather body and showing the error message
       locationNotFound.style.display = "flex";
       weatherBody.style.display = "none";
       console.log("error");
       return;
     }
+    // Else set the text values and images
     setTheValues(weather_response);
   } catch (error) {
     console.log(error);
   }
 }
 
-searchBtn.addEventListener("click", () => {
-  checkWeather(inputBox.value);
-});
 function setTheValues(weather_response) {
-  console.log(weather_response);
+  // setting the text values
   temperature.innerHTML =
     Math.round(weather_response.main.temp - 273.15) + "°C";
   weatherDesc.innerHTML = weather_response.weather[0].description;
   humidity.innerHTML = weather_response.main.humidity + " %";
   windSpeed.innerHTML = weather_response.wind.speed + "km/h";
+  // Hiding any error message and showing the weather body instead
   locationNotFound.style.display = "none";
   weatherBody.style.display = "flex";
 
+  setThePictures(weather_response);
+}
+
+function setThePictures(weather_response) {
   switch (weather_response.weather[0].main) {
     case "Clouds":
       weatherImg.src = "images/cloud.png";
